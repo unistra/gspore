84dd77d44ee929237d8c00f13e055485e18e5d17
geoffroybeck <geoffroy.beck@gmail.com>
2014-07-02 16:25:32 +0200

    correction in the holder

--- gspore/src/main/java/holder/Holder.groovy
+++ gspore/src/main/java/holder/Holder.groovy
@@ -10,13 +10,13 @@ class Holder {
 		
 	}
 	
-	public static Spore getClient(name,url){
+	public static Spore getClient(name,url,base_url=null){
 		Spore spore
 		
 		if (Spores.containsKey(name)){
 			spore = Spores[name]
 		}else{
-			spore = feed(url)
+			spore = feed(url,base_url)
 			Spores[name]=spore
 		}
 		return spore
